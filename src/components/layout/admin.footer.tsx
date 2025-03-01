6143cccce4efa146d8fa59879e6070e68fa73794
Hung Pham <hungphammanh2012003@gmail.com>
2025-03-01 17:52:16 +0700

    video step 11

---
+++ src/components/layout/admin.footer.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Layout } from 'antd';
+const AdminFooter = () => {
+    const { Footer } = Layout;
+    return (
+        <>
+            <Footer style={{ textAlign: 'center' }}>
+                EMT ©{new Date().getFullYear()} Created by @emt
+            </Footer>
+        </>
+    )
+}
+
+export default AdminFooter
