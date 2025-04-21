7aab42b18be0d3e9559ba4a98f7ba459007388b6
Hung Pham <hungphammanh2012003@gmail.com>
2025-04-21 17:44:08 +0700

    finish 19

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,5 @@ export const config = {
         // '/((?!auth).*)(.+)|/verify',
         // "/((?!api|_next/static|_next/image|favicon.ico|/|/auth).*)",
         '/((?!api|_next/static|_next/image|favicon.ico|auth|verify|$).*)',
-
-
     ],
 }
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,5 +1,9 @@
 'use server'
-import { signIn } from "@/auth";
+
+import { auth, signIn } from "@/auth";
+import { revalidateTag } from 'next/cache'
+import { sendRequest } from "./api";
+
 
 export async function authenticate(username: string, password: string) {
     try {
@@ -10,13 +14,14 @@ export async function authenticate(username: string, password: string) {
             redirect: false,
         })
         console.log(">>> check r: ", r)
-        return r
+        return r;
     } catch (error) {
         if ((error as any).name === "InvalidEmailPasswordError") {
             return {
                 error: (error as any).type,
                 code: 1
             }
+
         } else if ((error as any).name === "InactiveAccountError") {
             return {
                 error: (error as any).type,
@@ -24,7 +29,7 @@ export async function authenticate(username: string, password: string) {
             }
         } else {
             return {
-                error: "Internal server error.",
+                error: "Internal server error",
                 code: 0
             }
         }
@@ -32,6 +37,44 @@ export async function authenticate(username: string, password: string) {
     }
 }
 
+export const handleCreateUserAction = async (data: any) => {
+    const session = await auth();
+    const res = await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users`,
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${session?.user?.access_token}`,
+        },
+        body: { ...data }
+    })
+    revalidateTag("list-users")
+    return res;
+}
 
+export const handleUpdateUserAction = async (data: any) => {
+    const session = await auth();
+    const res = await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users`,
+        method: "PATCH",
+        headers: {
+            Authorization: `Bearer ${session?.user?.access_token}`,
+        },
+        body: { ...data }
+    })
+    revalidateTag("list-users")
+    return res;
+}
 
+export const handleDeleteUserAction = async (id: any) => {
+    const session = await auth();
+    const res = await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users/${id}`,
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${session?.user?.access_token}`,
+        },
+    })
 
+    revalidateTag("list-users")
+    return res;
+}
